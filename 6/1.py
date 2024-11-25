935cd1ad0b6c4de103d47705bd1a0059190a51f1
nonam00 <raisky.ilya3@yandex.ru>
2024-11-25 23:46:54 +0300

    6

---
+++ 6/1.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+screensize(10000, 10000)
+tracer(0)
+m = 10
+for i in range(100):
+    forward(10 * m)
+    right(180)
+    forward(10 * m)
+    right(198)
+input()
---
+++ 6/5.py
@@ -0,0 +1,8 @@
+from turtle import * 
+tracer(0)
+n = 10
+for i in range(n):
+    forward(10)
+    right(50)
+
+input()
